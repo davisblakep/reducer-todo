a2dd2f5b48c1cb374b14c2186331f942b297d5dd
Blake Davis <blakedavis@Blakes-MBP.lan1>
2020-06-17 19:32:57 -0400

    Created reducers folder along with todoReducer.js file to house initial state and plain reducer function

---
+++ todo/src/reducers/todoReducer.js
@@ -0,0 +1,22 @@
+
+
+export const initialState = [
+    {
+        item: 'Learn about reducers',
+        completed: false,
+        id: 3892987589
+    },
+    {
+        item: 'Smell the wall',
+        completed: false,
+        id: 3892989289
+    },
+]
+
+export const reducer = (state, action) => {
+    switch(action.type){
+        
+        default: 
+            return state;
+    }
+}
